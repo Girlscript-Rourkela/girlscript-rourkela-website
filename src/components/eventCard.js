29c226f15c6f3551793d69b0ccf72f572a9036b1
hemant <41hemu38@gmail.com>
2021-02-17 02:11:28 +0530

    made the card in event page

--- src/components/eventCard.js
+++ src/components/eventCard.js
@@ -10,19 +10,15 @@ import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({ 
   root: {
-    display: 'flex',
     marginTop:'2%',
-    flexDirection:'column',
-  },
-  details: {
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  content: {
-    flex: '1 0 auto',
+    height: 150,
+    width: 300
+    
   },
   cover: {
-    width: 60,
+    width: 65,
+    height:60,
+    margin: 'auto'
   }
 }));
 
@@ -44,7 +40,7 @@ export default function MediaControlCard(props) {
           title="Contemplative Reptile"
         />
         <CardContent className={classes.content}>
-          <Typography variant="h5" component="h5">
+          <Typography variant="h6" component="h6" >
             {props.title}
           </Typography>
           <Typography variant="subtitle1" color="textSecondary" component="p">
--- src/pages/events.js
+++ src/pages/events.js
@@ -6,23 +6,81 @@ const About = () => {
     <div
       style={{
         display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
+        //justifyContent: 'center',
+        //alignItems: 'center',
         height: '90vh',
         backgroundColor: '#ffd4a9',
       }}
     > 
-      <div style={{display:'block',paddingTop:5}}>
-      <EventCard title="Education Outreach Program" stats="5000+ Students Impacted" image="https://www.girlscript.tech/content/images/programs/educationoutreach.jpg" />
-      <EventCard title="GirlScript Summer Of Code" stats="3,500+ Participants" image="https://www.girlscript.tech/Scripts/images/causes/gssoc_logoNew.png" />
-      <EventCard title="LetsPy" stats="3,000+ Students Impacted" image="https://www.girlscript.tech/Scripts/images/causes/letspy_logoNew.png" />
-      <EventCard title="GirlScript India Summit" stats="2,500+ Students Impacted" image="https://www.girlscript.tech/Scripts/images/causes/india_summit_logoNew.png" />
-      <EventCard title="Uplift" stats="Guide to success " image="https://www.girlscript.tech/content/images/programs/uplift.png" />
-      <EventCard title="GS Cup" stats="Competition for the best" image="https://www.girlscript.tech/content/images/programs/cup.jpg" />
-      </div>
+<div>
 
+
+    <div class="flex-grid-2" style={{
+      display: 'flex',
+      flexFlow: 'row',
+      margin:'50px',
+      marginTop:'150px',
+      marginLeft:'100px'
+    }}>
+			<div class="brand-col" style={{
+        marginLeft:'20px',
+        textAlign:'center'
+      }} >
+      <EventCard title="Education Outreach Program" stats="5000+ Students Impacted" image="https://www.girlscript.tech/content/images/programs/educationoutreach.jpg" />
+			</div>
+			<div class="brand-col" style={{
+        marginLeft:'20px',
+        textAlign:'center'
+      }} >
+      <EventCard title="GirlScript Summer Of Code" stats="3,500+ Participants" image="https://www.girlscript.tech/Scripts/images/causes/gssoc_logoNew.png" />
+			</div>
+		</div>
+		
+		<div class="flex-grid-2" style={{
+      display: 'flex',
+      flexFlow: 'row',
+      margin: '50px',
+      marginLeft:'100px'
+    }} >
+			<div class="brand-col" style={{
+        marginLeft:'20px',
+        textAlign:'center'
+      }} >
+      <EventCard title="LetsPy" stats="3,000+ Students Impacted" image="https://www.girlscript.tech/Scripts/images/causes/letspy_logoNew.png" />
+			</div>
+			<div class="brand-col" style={{
+        marginLeft:'20px',
+        textAlign:'center'
+      }} >
+      <EventCard title="GirlScript India Summit" stats="2,500+ Students Impacted" image="https://www.girlscript.tech/Scripts/images/causes/india_summit_logoNew.png" />
+			</div>
+		</div>
+		
+		<div class="flex-grid-2" style={{
+      display: 'flex',
+      flexFlow: 'row',
+      margin:'50px',
+      marginLeft:'100px'
+    }} >
+			<div class="brand-col" style={{
+        marginLeft:'20px',
+        textAlign:'center'
+      }} >
+      <EventCard title="Uplift" stats="Guide to success " image="https://www.girlscript.tech/content/images/programs/uplift.png" />
+			</div>
+			<div class="brand-col" style={{
+        marginLeft:'20px',
+        textAlign:'center'
+      }} >
+      <EventCard title="GS Cup" stats="Competition for the best" image="https://www.girlscript.tech/content/images/programs/cup.jpg" />
+			</div>
+		</div>
+    </div>
+
+    <img src={Photo} alt="events photo" className="img vert-move" style={{
+      marginLeft:'15%',
       
-      <img src={Photo} alt="events photo" className="img vert-move" />
+    }} />
     </div>
   );
 };
