13b1f85bc58aef27088e6e64f28a893cb0acb103
hemant <41hemu38@gmail.com>
2021-02-18 02:36:03 +0530

    updated team members

--- src/components/teamCard.js
+++ src/components/teamCard.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { Card } from 'react-bootstrap'
 
 const CardExampleCardProps = (props) => (
-    <Card style={{ width: '18rem', padding:'0 5rem' }}>
+    <Card style={{  padding:'0 5rem' }}>
     <Card.Img variant="top" src={props.image} style={{
         height:'100px',
         width:'100px',
-        borderRadius:'100px',
-    }} />
+        borderRadius:'50%',
+        color:'#fff3e2' 
+        }} />
     <Card.Body>
     <Card.Title>{props.title}</Card.Title>
     <Card.Text>
--- src/pages/team.js
+++ src/pages/team.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import TeamCard from '../components/teamCard';
+import Photo1 from '../assets/1.jpg'
+import Photo2 from  '../assets/123 (2).jpg'
+import Photo3 from '../assets/WhatsApp Image 2021-02-16 at 4.32.51 PM.jpeg'
+import Photo4 from '../assets/1252.jpg'
+import Photo5 from '../assets/6.jpg'
+import Photo6 from '../assets/753.jpg'
+import Photo7 from '../assets/255.jpg'
+import Photo8 from '../assets/56.jpg'
+import Photo9 from '../assets/25.jpg'
+import Photo10 from '../assets/123.jpg'
+import Photo11 from '../assets/Raghav.jpg'
+import Photo12 from '../assets/2.jpg'
 
 const About = () => {
     return (
@@ -28,7 +40,7 @@ const About = () => {
                 flexDirection:'row',
                 justifyContent:'center'
                 }}>
-                <TeamCard name="Aniket Pal" title="Chapter Lead" image="https://www.girlscript.tech/Scripts/images/causes/gssoc_logoNew.png" />
+                <TeamCard name="Aniket Pal" title="Chapter Lead" image={Photo8} />
                 </div>
             </div>
 
@@ -41,8 +53,8 @@ const About = () => {
                 flexDirection:'row',
                 justifyContent:'center'
                 }}>
-                <TeamCard name="Hemant Bajaj" title="Technical Lead & Core Team" image="https://www.girlscript.tech/Scripts/images/causes/gssoc_logoNew.png" />
-                <TeamCard name="Sneha Mallik" title="Technical Lead" image="https://www.girlscript.tech/Scripts/images/causes/gssoc_logoNew.png" />
+                <TeamCard name="Hemant Bajaj" title="Technical Team" image={Photo1} />
+                <TeamCard name="Sneha Mallik" title="Technical Team" image={Photo10} />
                 </div>
             </div>
             
@@ -55,9 +67,9 @@ const About = () => {
                 flexDirection:'row',
                 justifyContent:'center'
                 }}>
-                <TeamCard name="Rohan" title="Management core team" image="https://www.girlscript.tech/Scripts/images/causes/gssoc_logoNew.png" />
-                <TeamCard name="Sayantan" title="Management core team" image="https://www.girlscript.tech/Scripts/images/causes/gssoc_logoNew.png" />
-                <TeamCard name="Parwin" title="Management core team" image="https://www.girlscript.tech/Scripts/images/causes/gssoc_logoNew.png" />
+                <TeamCard name="Rohan" title="Management  team" image={Photo4} />
+                <TeamCard name="Sayantan" title="Management  team" image={Photo9}/>
+                <TeamCard name="Parwin" title="Management  team" image={Photo2} />
                 </div>
             </div>
             
@@ -70,10 +82,24 @@ const About = () => {
                 flexDirection:'row',
                 justifyContent:'center'
                 }}>
-                <TeamCard name="Raghav Bajaj" title="Technical Lead" image="https://www.girlscript.tech/Scripts/images/causes/gssoc_logoNew.png" />
-                <TeamCard name=" Bajaj" title="Technical Lead" image="https://www.girlscript.tech/Scripts/images/causes/gssoc_logoNew.png" />
-                <TeamCard name="Hemant Bajaj" title="Technical Lead" image="https://www.girlscript.tech/Scripts/images/causes/gssoc_logoNew.png" />
-                <TeamCard name="Hemant Bajaj" title="Technical Lead" image="https://www.girlscript.tech/Scripts/images/causes/gssoc_logoNew.png" />
+                <TeamCard name="Raghav Bajaj" title="Design Team" image={Photo11} />
+                <TeamCard name="Shantanu" title="Design Team" image={Photo3} />
+                <TeamCard name="Sai Ashish" title="Design Team" image={Photo12} />
+                <TeamCard name="jon_snow" title="Design Team" image={Photo5} />
+                </div>
+            </div>
+
+            <div style={{
+                margin:'40px'
+            }} >
+                <h1 style={{textAlign:'center',margin:'40px'}} >Content Team</h1>
+                <div style={{
+                display:'flex',
+                flexDirection:'row',
+                justifyContent:'center'
+                }}>
+                <TeamCard name="Sumana Basu" title="Content Team" image={Photo7} />
+                <TeamCard name="sat_panda" title="Content Team" image={Photo6} />
                 </div>
             </div>
 
