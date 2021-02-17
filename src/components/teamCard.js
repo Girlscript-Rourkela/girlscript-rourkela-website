1c54fee816965ba8dd76ba4c2d11a6184394a6e8
hemant <41hemu38@gmail.com>
2021-02-18 02:40:38 +0530

    centered memojis

--- src/components/teamCard.js
+++ src/components/teamCard.js
@@ -6,12 +6,21 @@ const CardExampleCardProps = (props) => (
     <Card.Img variant="top" src={props.image} style={{
         height:'100px',
         width:'100px',
+        margin:'0 30px',
         borderRadius:'50%',
-        color:'#fff3e2' 
+        color:'#fff3e2',
+        justifyContent:'center',
+        alignItems:'center'
         }} />
     <Card.Body>
-    <Card.Title>{props.title}</Card.Title>
-    <Card.Text>
+    <Card.Title  style={{
+        textAlign:'center'
+    }}>
+        {props.title}
+        </Card.Title>
+    <Card.Text style={{
+        textAlign:'center'
+    }}>
         {props.name}
     </Card.Text>
     </Card.Body>
